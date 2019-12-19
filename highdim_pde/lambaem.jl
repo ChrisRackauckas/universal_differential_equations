60134822a55079c8839d272ec5ff1dfb0ff53587
Christopher Rackauckas <accounts@chrisrackauckas.com>
2019-12-19 14:09:04 -0500

    Update lambaem.jl

--- highdim_pde/lambaem.jl
+++ highdim_pde/lambaem.jl
@@ -1,20 +1,19 @@
-using NeuralNetDiffEq, StochasticDiffEq, Flux, LinearAlgebra, Statistics
+using Flux, StochasticDiffEq, LinearAlgebra, Statistics, Test, NeuralNetDiffEq
 
 d = 100 # number of dimensions
 x0 = fill(0.0f0,d)
 tspan = (0.0f0, 1.0f0)
-dt = 0.2
 m = 100 # number of trajectories (batch size)
 λ = 1.0f0
 #
 g(X) = log(0.5f0 + 0.5f0*sum(X.^2))
 f(X,u,σᵀ∇u,p,t) = -λ*sum(σᵀ∇u.^2)
-μ(X,p,t) = -10.0f0.*(X./X)  #Vector d x 1 λ
+μ(X,p,t) = zero(X)  #Vector d x 1 λ
 σ(X,p,t) = Diagonal(sqrt(2.0f0)*ones(Float32,d)) #Matrix d x d
 prob = TerminalPDEProblem(g, f, μ, σ, x0, tspan)
 
 hls = 10 + d #hidden layer size
-opt = Flux.ADAM(0.01)  #optimizer
+opt = Flux.ADAM(0.03)  #optimizer
 #sub-neural network approximating solutions at the desired point
 u0 = Flux.Chain(Dense(d,hls,relu),
                 Dense(hls,hls,relu),
@@ -26,12 +25,8 @@ u0 = Flux.Chain(Dense(d,hls,relu),
                   Dense(hls,d))
 pdealg = NNPDENS(u0, σᵀ∇u, opt=opt)
 #
-
-@time ans = solve(prob, pdealg, verbose=true, maxiters=100, trajectories=m,
-                            alg=EM(), dt=dt, pabstol = 1f-2)
-
-@time ans = solve(prob, pdealg, verbose=true, maxiters=100, trajectories=m,
-                            alg=LambaEM(), dt=dt, pabstol = 1f-2)
+@time ans = solve(prob, pdealg, verbose=true, maxiters=500, trajectories=m,
+                            alg=LambaEM(), pabstol = 1f-2, reltol=1e-4, abstol=1e-4)
 
 T = tspan[2]
 MC = 10^5
