1c3dee8554248fccbdd61acd34a9df5bc49b6011
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-12-20 07:26:29 -0500

    Adjust simulation parameters and plot buoyancy

--- Climate/DataGeneration/rayleigh_taylor_instability_3d.jl
+++ Climate/DataGeneration/rayleigh_taylor_instability_3d.jl
@@ -1,23 +1,25 @@
-using Printf
+using Plots, Printf
 
 using Oceananigans
 using Oceananigans.Diagnostics
 using Oceananigans.OutputWriters
 
 # Model parameters
-N = 32  # Number of grid points in each dimension.
+# N = 32  # Number of grid points in each dimension.
+Nx = Nz = 128
+Ny = 2
 L = 1   # Physical length of each dimension.
 
-κ = 0.05  # Diffusivity
+κ = 1e-4  # Diffusivity
 ν = κ     # Assuming Prandtl number Pr = 1
 
-end_time = 1
+end_time = 2
 
 # Set up model
 model = Model(
            architecture = CPU(),
              float_type = Float64,
-                   grid = RegularCartesianGrid(size=(N, N, N), x=(-L/2, L/2), y=(-L/2, L/2), z=(-L/2, L/2)),
+                   grid = RegularCartesianGrid(size=(Nx, Ny, Nz), x=(-L/2, L/2), y=(-L/2, L/2), z=(-L/2, L/2)),
                 tracers = (:b,),
                coriolis = nothing,
                buoyancy = BuoyancyTracer(),
@@ -27,7 +29,15 @@ model = Model(
 
 # Setting initial conditions
 ε(σ) = σ * randn()  # Noise
-b₀(x, y, z) = 1 * (z < 0) + -1 * (z > 0) + ε(1e-8)
+
+# b₀(x, y, z) = 1 * (z < 0) + -1 * (z > 0) + ε(1e-2)
+
+@inline interface(x, y, z) = 0.05sin(6π*x)
+@inline function b₀(x, y, z)
+    z > interface(x, y, z) && return -1
+    return 1
+end
+
 set!(model, b=b₀)
 
 # Add JLD2 output writer for 3D fields.
@@ -40,7 +50,7 @@ fields = Dict(
 
 model.output_writers[:fields] =
     JLD2OutputWriter(model, fields, dir=".", prefix="rayleigh_taylor_3d_fields",
-                     interval=0.1, force=true, verbose=true)
+                     interval=1, force=true, verbose=true)
 
 # Add JLD2 output writer for horizontal averages.
  u̅ = HorizontalAverage(model.velocities.u; return_type=Array)
@@ -57,10 +67,10 @@ model.output_writers[:fields] =
 
 model.output_writers[:horizontal_averages] =
     JLD2OutputWriter(model, profiles, dir=".", prefix="rayleigh_taylor_3d_horizontal_averages",
-                     interval=0.01, force=true, verbose=true)
+                     interval=0.1, force=true, verbose=true)
 
 # Set up adaptive time stepping.
-wizard = TimeStepWizard(cfl=0.1, Δt=1e-6, max_change=1.2, max_Δt=1e-2)
+wizard = TimeStepWizard(cfl=0.1, Δt=1e-6, max_change=1.2, max_Δt=1e-3)
 
 # Set up CFL diagnostics.
 cfl = AdvectiveCFL(wizard)
@@ -68,6 +78,22 @@ cfl = AdvectiveCFL(wizard)
 # Number of time steps to perform at a time before printing a progress
 # statement and updating the adaptive time step.
 Ni = 20
+
+function plot_buoyancy(model)
+    t_str = @sprintf("t = %.2f", model.clock.time)
+    xC, zC = model.grid.xC, model.grid.zC
+    
+    b_profile = plot(b̅(model)[2:end-1], zC, title=t_str, label="", xlabel="buoyancy", ylabel="z",
+                     xlims=(-1, 1), ylims=(-L/2, L/2), aspect_ratio=:equal)
+    
+    j½ = Int(model.grid.Ny/2)
+    b_xz_data = interior(model.tracers.b)[:, j½, :]'
+    b_slice = heatmap(xC, zC, b_xz_data, title="buoyancy slice", color=:balance,
+                      xlims=(-L/2, L/2), ylims=(-L/2, L/2), clims=(-1, 1), aspect_ratio=:equal)
+    
+    display(plot(b_profile, b_slice, layout=(1, 2), show=true))
+    return nothing
+end
 
 while model.clock.time < end_time
     walltime = @elapsed time_step!(model; Nt=Ni, Δt=wizard.Δt)
@@ -83,9 +109,11 @@ while model.clock.time < end_time
     # Calculate a new adaptive time step.
     update_Δt!(wizard, model)
 
+    plot_buoyancy(model)
+
     # Print progress statement.
     i, t = model.clock.iteration, model.clock.time
-    @printf("[%06.2f%%] i: %d, t: %5.2e days, umax: (%6.3e, %6.3e, %6.3e), CFL: %6.4g, next Δt: %8.5e, ⟨wall time⟩: %s\n",
+    @printf("[%05.2f%%] i: %d, t: %.2e, umax: (%.3e, %.3e, %.3e), CFL: %.4e, next Δt: %.2e, ⟨wall time⟩: %s\n",
             progress, i, t, umax, vmax, wmax, cfl(model), wizard.Δt, prettytime(walltime / Ni))
 end
 
